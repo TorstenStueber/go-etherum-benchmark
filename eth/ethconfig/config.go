34c286df68b53e15d1e8b152ac77ca71614c3890
Torsten Stüber <torsten.stuber@gmail.com>
2022-06-28 06:02:36 +0200

    Change settings to make it more suitable for benchmarking

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -89,10 +89,10 @@ var Defaults = Config{
 		Recommit: 3 * time.Second,
 	},
 	TxPool:        core.DefaultTxPoolConfig,
-	RPCGasCap:     50000000,
+	RPCGasCap:     5000000000,
 	RPCEVMTimeout: 5 * time.Second,
 	GPO:           FullNodeGPO,
-	RPCTxFeeCap:   1, // 1 ether
+	RPCTxFeeCap:   1000, // 1 ether
 }
 
 func init() {
